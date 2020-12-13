f5c16c19c46bce163815ab63fed4ae17055b773f
karolbury98 <37350294+karolbury98@users.noreply.github.com>
2020-12-13 13:32:47 +0100

    order list, customer list, cart, product details update

--- front/src/app/components/cart/cart.component.ts
+++ front/src/app/components/cart/cart.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
+import {faShoppingBasket} from '@fortawesome/free-solid-svg-icons';
+import {faTrashAlt} from '@fortawesome/free-solid-svg-icons';
+import {faPlusCircle} from '@fortawesome/free-solid-svg-icons';
+import {faMinusCircle} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-cart',
@@ -6,6 +12,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  faArrowLeft = faArrowLeft;
+  faCheckCircle = faCheckCircle;
+  faShoppingBasket=faShoppingBasket;
+  faTrashAlt = faTrashAlt;
+  faPlusCircle = faPlusCircle;
+  faMinusCircle = faMinusCircle;
 
   constructor() { }
 
--- front/src/app/components/order-edit/order-edit.component.ts
+++ front/src/app/components/order-edit/order-edit.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import {faTrashAlt} from '@fortawesome/free-solid-svg-icons';
+import {faPlusCircle} from '@fortawesome/free-solid-svg-icons';
+import {faMinusCircle} from '@fortawesome/free-solid-svg-icons';
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
+
+
 
 @Component({
   selector: 'app-order-edit',
@@ -6,7 +14,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-edit.component.css']
 })
 export class OrderEditComponent implements OnInit {
-
+  faPencilAlt = faPencilAlt;
+  faTrashAlt = faTrashAlt;
+  faMinusCircle = faMinusCircle;
+  faPlusCircle = faPlusCircle;
+  faArrowLeft = faArrowLeft;
+  faCheckCircle = faCheckCircle;
   constructor() { }
 
   ngOnInit(): void {
